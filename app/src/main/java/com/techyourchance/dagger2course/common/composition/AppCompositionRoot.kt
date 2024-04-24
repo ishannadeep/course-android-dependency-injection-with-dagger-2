3668d3c53b67f10d0d8061dfa073b0e15ad5591e
Ishan <ishank@nvision.lk>
2024-04-24 16:45:52 +0530

    feat: AppCompositionRoot make retrofit object create only when needed with thread safe

--- app/src/main/java/com/techyourchance/dagger2course/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/composition/AppCompositionRoot.kt
@@ -1,18 +1,27 @@
 package com.techyourchance.dagger2course.common.composition
 
+import androidx.annotation.UiThread
 import com.techyourchance.dagger2course.Constants
 import com.techyourchance.dagger2course.networking.StackoverflowApi
 import com.techyourchance.dagger2course.questions.FetchQuestionDetailsUseCase
 import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
+@UiThread
 class AppCompositionRoot {
 
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    private var _retrofit: Retrofit? = null
+
+    private val retrofit: Retrofit = if (_retrofit == null) {
+        _retrofit = Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        _retrofit!!
+    } else {
+        _retrofit!!
+    }
+
 
     private val stackoverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
 
