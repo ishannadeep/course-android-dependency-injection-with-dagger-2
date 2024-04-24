a1274a627d620dc4c2f035918b45a4f20a43dec7
Ishan <ishank@nvision.lk>
2024-04-24 17:20:19 +0530

    feat: AppCompositionRoot make retrofit object create by lazy delegate

--- app/src/main/java/com/techyourchance/dagger2course/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/composition/AppCompositionRoot.kt
@@ -7,19 +7,15 @@ import com.techyourchance.dagger2course.questions.FetchQuestionDetailsUseCase
 import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
 @UiThread
 class AppCompositionRoot {
 
-    private var _retrofit: Retrofit? = null
-
-    private val retrofit: Retrofit = if (_retrofit == null) {
-        _retrofit = Retrofit.Builder()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-        _retrofit!!
-    } else {
-        _retrofit!!
     }
 
 
